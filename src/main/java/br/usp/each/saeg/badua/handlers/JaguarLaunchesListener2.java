99458c55c0ffb3a3dcd88bdf15111793e9e0a52a
luiz.baptistao <luiz.baptistao@gmail.com>
2014-10-01 17:01:22 -0300

    baduino alpha

---
+++ src/main/java/br/usp/each/saeg/badua/handlers/JaguarLaunchesListener2.java
@@ -0,0 +1,41 @@
+package br.usp.each.saeg.badua.handlers;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchesListener2;
+
+import br.usp.each.saeg.badua.utils.ProjectUtils;
+
+public class JaguarLaunchesListener2 implements ILaunchesListener2 {
+
+	private IProject project;
+
+	public JaguarLaunchesListener2(IProject project) {
+		this.project = project;
+	}
+
+	@Override
+	public void launchesRemoved(ILaunch[] launches) {
+	}
+
+	@Override
+	public void launchesAdded(ILaunch[] launches) {
+	}
+
+	@Override
+	public void launchesChanged(ILaunch[] launches) {
+	}
+
+	@Override
+	public void launchesTerminated(ILaunch[] launches) {
+		System.out.println("LAUNCHES TERMINATED");
+		try {
+			project.refreshLocal(IResource.DEPTH_ONE, null);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
