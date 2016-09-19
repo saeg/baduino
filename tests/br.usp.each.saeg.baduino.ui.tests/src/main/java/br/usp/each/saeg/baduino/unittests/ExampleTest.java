c7f4543eaa3e80d862b768e169bcac209ac30a6c
marioconcilio <marioconcilio@gmail.com>
2016-09-19 16:39:10 -0300

    tests

---
+++ tests/br.usp.each.saeg.baduino.ui.tests/src/main/java/br/usp/each/saeg/baduino/unittests/ExampleTest.java
@@ -0,0 +1,14 @@
+package br.usp.each.saeg.baduino.unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
